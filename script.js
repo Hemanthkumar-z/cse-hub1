0634658dfd61557df0a5ed9c1e769213c84901f3
Hemanthkumar-z <hemanthkumar7489@gmail.com>
2026-04-07 09:47:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Scroll animation
+const cards = document.querySelectorAll('.card');
+
+window.addEventListener('scroll', () => {
+  const trigger = window.innerHeight * 0.85;
+
+  cards.forEach(card => {
+    const top = card.getBoundingClientRect().top;
+
+    if(top < trigger){
+      card.classList.add('show');
+    }
+  });
+});
